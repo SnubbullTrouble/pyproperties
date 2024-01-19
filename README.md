310beb8bbf1244d8cd92d6ae68b3707c88b3dc13
SnubbullTrouble <snubbulltrouble@gmail.com>
2024-01-18 23:01:35 -0500

    redone using properties instead of decorators

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# pyproperties
-A python module to read and write application properties to a file.
+# PyProperties
+PPyProperties is a Python module that helps you easily read and write save-state data.
+
+## Cloning
+To clone the repository use the following command:
+
+```
+git clone https://github.com/SnubbullTrouble/pyproperties.git
+```
+
+## PyProperties Wiki
+Read the [PyProperties Wiki Page](https://github.com/SnubbullTrouble/pyproperties/wiki).
+
--- pyproperties.py
+++ pyproperties.py
@@ -1,21 +1,31 @@
+import inspect
 import os
-import toml
+from typing import Any, Optional
+import tomlkit
 
-#decorator that turns each field into a class property and assigns each one a getter
-def propertygetter(cls):
-    for x in cls.properties:
-        getter = lambda self, x=x: self.properties[x]
-        setattr(cls, "{}".format(x), property(getter))
-    return cls
-
-@propertygetter
 class PyProperties(object):
     properties_file = ".properties.toml"
-    properties = toml.load(properties_file)["Properties"] if os.path.exists(properties_file) else None
+    properties_group = "Properties"
+    properties = {}
 
-    def get_properties(self):
-        return self.properties
+    def __init__(self, file_location: str = os.getcwd()):
+        self.properties_file = '/'.join([file_location, self.properties_file])
+        self.properties = tomlkit.load(open(self.properties_file))[self.properties_group] if os.path.exists(self.properties_file) else {}
+        pass
 
-    def write_properties(self, dict):
-        with open(self.properties_file, 'w') as file:
-            toml.dump({"Properties": dict}, file)
+    def __getattr__(self, __name: str) -> Optional[str]:
+        if __name in self.properties.keys():
+            return self.properties[__name]
+        else:
+            return None
+        
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        if __name in ["properties_file", "properties_group", "properties"]:
+            pass
+        else:
+            self.properties[__name] = str(__value)
+
+    def write_properties(self):
+        if len(self.properties) > 0:
+            with open(self.properties_file, 'w') as file:
+                tomlkit.dump({self.properties_group: self.properties}, file)
